8c4551bf022ad047a2d78e611f95f9b89b0e915d
Alexis Facques <alexis.facques@om.fr>
2022-11-24 15:24:39 +0100

    Add batch bolt.

--- boto_iterators/bolts/__init__.py
+++ boto_iterators/bolts/__init__.py
@@ -1,6 +1,7 @@
 """Iterator bolts."""
 
 # From local modules:
+from .batch import batch
 from .boto_assume_role import boto_assume_role
 from .boto_method import boto_method
 from .boto_paginator import boto_paginator
---
+++ boto_iterators/bolts/batch.py
@@ -0,0 +1,39 @@
+"""A generic bolt to transform the output of other bolts."""
+from itertools import islice
+import logging
+from typing import Iterator, Tuple
+
+# From local modules:
+from ..type_hints import BoltItem, IteratorBolt, Result
+
+
+LOGGER = logging.getLogger()
+
+
+def batch(BatchSize: int) -> IteratorBolt:
+    """
+    Configure a Chain bolt that yields the output of the 'Parser' function.
+
+    Function factory.
+
+    :param Parser: a function that takes the bolt input as argument,
+                   and return a transformed result. This result is yield
+                   by the Iterator Chain.
+
+    :return: an Iterator Chain bolt reading anything and yielding the transformed result of 'parser'.
+    """
+    def bolt(BoltItems: Iterator[BoltItem], *_args, **_kwargs) -> Iterator[BoltItem]:
+        """
+        Transform all 'items' to the output of 'parser'.
+
+        :param items: the items to transform.
+
+        :return: an enumeration of the results of the 'parser' function.
+        """
+        while True:
+            batched_records: Tuple[Result, ...] = tuple(islice((item for item, prev in BoltItems), BatchSize))
+            if not batched_records:
+                return
+            yield ({'Records': batched_records}, tuple())
+
+    return bolt
